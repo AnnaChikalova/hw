279c6da6044b27b27d5b436e85def8d8fe1a63e9
AnnaChikalova <av.chikalova@gmail.com>
2023-03-29 20:49:30 +0300

    hw1

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -45,7 +45,7 @@ export const friendMessage0: MessageType = {
         name: 'Friend Name', // можно менять
     },
     message: {
-        text: 'зеркальное сообщение для тренировки css', // можно менять
+        text: 'Hello))', // можно менять
         time: '22:00', // можно менять
     },
 }
