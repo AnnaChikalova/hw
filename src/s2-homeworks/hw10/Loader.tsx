d0acbc1eb8b10c5598e24e6f784e0947209ea2a9
AnnaChikalova <av.chikalova@gmail.com>
2023-04-24 19:59:11 +0300

    hw10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,14 @@
 import s from './Loader.module.css'
+import {CircularProgress} from "@mui/material";
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => {
+    return (
+            <div className={s.loader}>
+                крутилка :)
+                    <CircularProgress color="inherit" />
+            </div>
+
+
+    )
+}
+
