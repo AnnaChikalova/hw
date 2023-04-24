fdb73f0bc1134f2338df535cc0a29886c3861278
AnnaChikalova <av.chikalova@gmail.com>
2023-04-24 19:59:11 +0300

    hw10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -5,6 +5,7 @@ import {loadingAC} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
 import {Loader} from './Loader'
+import s from './Loader.module.css'
 
 /*
 * 1 - в файле loadingReducer.ts дописать типы и логику
@@ -14,12 +15,15 @@ import {Loader} from './Loader'
 * */
 
 const HW10 = () => {
+    const dispatch=useDispatch()
+    let isLoading= useSelector <AppStoreType, boolean>(state => state.loading.isLoading)
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
+        dispatch(loadingAC(true))
         // dispatch
-
+        setTimeout(()=>(dispatch(loadingAC(false))), 1500)
         // setTimeout
     }
 
@@ -27,18 +31,19 @@ const HW10 = () => {
         <div id={'hw10'}>
             <div className={s2.hwTitle}>Homework #10</div>
 
-            <div className={s2.hw}>
+            <div className={s.buttonLoader}>
                 {isLoading ? (
                     <div id={'hw10-loading'}>
                         <Loader/>
                     </div>
                 ) : (
-                    <SuperButton
-                        id={'hw10-button-start-loading'}
-                        onClick={setLoading}
-                    >
-                        Set loading...
-                    </SuperButton>
+                         <SuperButton
+                             id={'hw10-button-start-loading'}
+                             onClick={setLoading}
+                         >
+                             Set loading
+                         </SuperButton>
+
                 )}
             </div>
         </div>
